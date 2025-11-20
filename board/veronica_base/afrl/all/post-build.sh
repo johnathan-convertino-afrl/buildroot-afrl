612ff6970205f19334436977d1c6091ab2bbbc3a
Jay Convertino <johnathan.convertino.1@us.af.mil>
2025-11-19 21:26:05 -0500

    move veronica out of rfbase into veronica_base, add rootfs mount

--- board/veronica_base/afrl/all/post-build.sh
+++ board/veronica_base/afrl/all/post-build.sh
@@ -8,3 +8,7 @@ set -e
 grep -qE '^::sysinit:/bin/mount -t debugfs' ${TARGET_DIR}/etc/inittab || \
 sed -i '/hostname/a\
 ::sysinit:/bin/mount -t debugfs none /sys/kernel/debug/' ${TARGET_DIR}/etc/inittab
+
+grep -q /dev/mmcblk0p2 ${TARGET_DIR}/etc/fstab || echo "/dev/mmcblk0p2 /mnt/rootfs ext4 defaults 0 2" >> ${TARGET_DIR}/etc/fstab
+
+mkdir -p ${TARGET_DIR}/mnt/rootfs
